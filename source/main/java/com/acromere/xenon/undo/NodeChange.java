78a74346cefb1dd46fb42e80a75334483f7e5a9f
Mark Soderquist <msoderquist@mastercontrol.com>
2026-09-22 11:41:50 -0600

    Add `@Getter` annotations to `NodeChange` fields for cleaner access and remove redundant getter methods

--- source/main/java/com/acromere/xenon/undo/NodeChange.java
+++ source/main/java/com/acromere/xenon/undo/NodeChange.java
@@ -2,8 +2,8 @@ package com.acromere.xenon.undo;
 
 import com.acromere.data.DataNode;
 import lombok.CustomLog;
+import lombok.Getter;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -12,22 +12,24 @@ public class NodeChange {
 
 	private static final String CAPTURE_UNDO_CHANGES = NodeChange.class.getName() + ":capture-undo-changes";
 
-	//private static final String PUBLISH_UNDO_CHANGES = NodeChange.class.getName() + ":publish-undo-changes";
-
-	//private static final String CAPTURE_REDO_CHANGES = NodeChange.class.getName() + ":capture-redo-changes";
-
 	private static final boolean DEFAULT_CAPTURE_UNDO_CHANGES = false;
 
+	@Getter
 	private final DataNode node;
 
+	@Getter
 	private final String key;
 
+	@Getter
 	private final Object oldValue;
 
+	@Getter
 	private final Object newValue;
 
+	@Getter
 	private final boolean redo;
 
+	@Getter
 	private final List<NodeChange> changes;
 
 	public NodeChange( DataNode node, String key, Object oldValue, Object newValue ) {
@@ -43,13 +45,22 @@ public class NodeChange {
 		this.changes = List.of( this );
 	}
 
-	NodeChange( List<NodeChange> changes ) {
-		this.node = null;
-		this.key = null;
-		this.oldValue = null;
-		this.newValue = null;
-		this.redo = false;
-		this.changes = new ArrayList<>( changes );
+	public String toString() {
+		return node + " " + key + "=" + oldValue + " -> " + newValue;
+	}
+
+	@Override
+	public boolean equals( Object other ) {
+		if( !(other instanceof NodeChange that) ) return false;
+		if( this == that ) return true;
+		if( this.getClass() != other.getClass() ) return false;
+		return Objects.equals( this.node, that.node ) && Objects.equals( this.key, that.key ) && Objects.equals( oldValue, that.oldValue ) && Objects.equals( newValue, that.newValue );
+	}
+
+	@Override
+	public int hashCode() {
+		if( this.node != null ) return Objects.hash( node, key, oldValue, newValue );
+		return super.hashCode();
 	}
 
 	public static boolean isCaptureUndoChanges( DataNode node ) {
@@ -58,56 +69,6 @@ public class NodeChange {
 
 	public static void setCaptureUndoChanges( DataNode node, boolean capture ) {
 		node.setValue( NodeChange.CAPTURE_UNDO_CHANGES, capture );
-	}
-
-	DataNode getNode() {
-		return node;
-	}
-
-	String getKey() {
-		return key;
-	}
-
-	Object getOldValue() {
-		return oldValue;
-	}
-
-	Object getNewValue() {
-		return newValue;
-	}
-
-	public boolean isRedo() {
-		return redo;
-	}
-
-	List<NodeChange> getChanges() {
-		return changes;
-	}
-
-	public String toString() {
-		return node + " " + key + "=" + oldValue + " -> " + newValue;
-	}
-
-	@Override
-	public boolean equals( Object other ) {
-		if( this == other ) return true;
-		if( other == null || getClass() != other.getClass() ) return false;
-
-		// FIXME Should only have to compare the change lists
-
-		NodeChange that = (NodeChange)other;
-		//if( this.node != null ) {
-		return Objects.equals( this.node, that.node ) && Objects.equals( this.key, that.key ) && Objects.equals( oldValue, that.oldValue ) && Objects.equals( newValue, that.newValue );
-		//}
-
-		//return true;
-		//return Objects.equals( this.changes, that.changes );
-	}
-
-	@Override
-	public int hashCode() {
-		if( this.node != null ) return Objects.hash( node, key, oldValue, newValue );
-		return super.hashCode();
 	}
 
 }
